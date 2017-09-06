3467532e6756db0b37b29e8b22ea0ff5b273c997
Noah Saso <noahsaso@gmail.com>
2017-09-05 20:24:55 -0700

    Updated to work with student ID

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ function puts(error, stdout, stderr) { console.log(stdout, stderr); }
 
 function dev_log(string) { if (process.env.USER == 'noah') { console.log(string); } }
 
-function checkAccountExists(username, callback) {
-  var query = "./grades.py -x \"" + username + "\"";
+function checkAccountExists(student_id, callback) {
+  var query = "./grades.py -x \"" + student_id + "\"";
   dev_log("Running: " + query);
   exec(query, function (error, stdout, stderr) {
     var response = stdout.trim();
@@ -24,8 +24,8 @@ function checkAccountExists(username, callback) {
   });
 }
 
-function validAccountPassword(username, password, callback) {
-  var query = "./grades.py -z \"" + config.salt + "\" -v '" + JSON.stringify({ username: username, password: password }) + "'";
+function validAccountPassword(student_id, password, callback) {
+  var query = "./grades.py -z \"" + config.salt + "\" -v '" + JSON.stringify({ student_id: student_id, password: password }) + "'";
   dev_log("Running: " + query);
   exec(query, function (error, stdout, stderr) {
     var valid = stdout.trim() == '1';
@@ -41,8 +41,8 @@ function addAccount(user_data, callback) {
   });
 }
 
-function validICAccount(username, password, callback) {
-  var query = "./grades.py -i '" + JSON.stringify({ username: username, password: password }) + "'";
+function validICAccount(username, student_id, password, callback) {
+  var query = "./grades.py -i '" + JSON.stringify({ username: username, student_id: student_id, password: password }) + "'";
   dev_log("Running: " + query);
   exec(query, function (error, stdout, stderr) {
     var response = stdout.trim();
@@ -56,8 +56,8 @@ function validICAccount(username, password, callback) {
   });
 }
 
-function sendGrades(username, callback) {
-  var query = "./grades.py -z \"" + config.salt + "\" -g \"" + username + "\"";
+function sendGrades(student_id, callback) {
+  var query = "./grades.py -z \"" + config.salt + "\" -g \"" + student_id + "\"";
   dev_log("Running: " + query);
   exec(query, puts);
 }
@@ -88,18 +88,18 @@ router.post('/signup', function (req, res, next) {
   delete data.first_name;
   delete data.last_name;
 
-  checkAccountExists(data.username, function (exists, error) {
+  checkAccountExists(data.student_id, function (exists, error) {
     if (exists) {
-      res.send(JSON.stringify({ status: 'error', message: 'An account with that username is already registered. Please use the enable form on the "Edit Account" page if your account is disabled.' }));
+      res.send(JSON.stringify({ status: 'error', message: 'An account with that Student ID is already registered. Please use the enable form on the "Edit Account" page if your account is disabled.' }));
     } else {
-      validICAccount(data.username, data.password, function (valid, error) {
+      validICAccount(data.username, data.student_id, data.password, function (valid, error) {
         if (error.length > 0) {
           res.send(JSON.stringify({ status: 'error', message: error }));
         } else {
           if (valid) {
             addAccount(data, function () {
               res.send(JSON.stringify({ status: 'ok', message: 'You have been successfully registered. You will now receive notifications to the email you provided within roughly 30 minutes of a grade change.' }));
-              sendGrades(data.username);
+              sendGrades(data.student_id);
             });
           } else {
             res.send(JSON.stringify({ status: 'error', message: 'This is not a valid Infinite Campus account.' }));
@@ -122,11 +122,11 @@ router.post('/enable', function (req, res, next) {
   
   res.setHeader('Content-Type', 'application/json');
 
-  validAccountPassword(data.username, data.password, function (valid) {
+  validAccountPassword(data.student_id, data.password, function (valid) {
     if (!valid) {
-      res.send(JSON.stringify({ status: 'error', message: 'This username and password combination is incorrect.' }));
+      res.send(JSON.stringify({ status: 'error', message: 'These credentials are incorrect.' }));
     } else {
-      exec("./grades.py -e '" + data.username + "'", function (error, stdout, stderror) {
+      exec("./grades.py -e '" + data.student_id + "'", function (error, stdout, stderror) {
         res.send(JSON.stringify({ status: 'ok', message: 'Your account has been enabled.' }));
       });
     }
@@ -144,11 +144,11 @@ router.post('/disable', function (req, res, next) {
   
   res.setHeader('Content-Type', 'application/json');
 
-  validAccountPassword(data.username, data.password, function (valid) {
+  validAccountPassword(data.student_id, data.password, function (valid) {
     if (!valid) {
-      res.send(JSON.stringify({ status: 'error', message: 'This username and password combination is incorrect.' }));
+      res.send(JSON.stringify({ status: 'error', message: 'These credentials are incorrect.' }));
     } else {
-      exec("./grades.py -d '" + data.username + "'", function (error, stdout, stderror) {
+      exec("./grades.py -d '" + data.student_id + "'", function (error, stdout, stderror) {
         res.send(JSON.stringify({ status: 'ok', message: 'Your account has been disabled.' }));
       });
     }
@@ -166,16 +166,16 @@ router.post('/update', function (req, res, next) {
 
   res.setHeader('Content-Type', 'application/json');
   
-  validAccountPassword(data.username, data.old_password, function (valid) {
+  validAccountPassword(data.student_id, data.old_password, function (valid) {
     if (!valid) {
-      res.send(JSON.stringify({ status: 'error', message: 'This username and password combination is incorrect.' }));
+      res.send(JSON.stringify({ status: 'error', message: 'These credentials are incorrect.' }));
     } else {
-      validICAccount(data.username, data.new_password, function (valid, error) {
+      validICAccount(data.username, data.student_id, data.new_password, function (valid, error) {
         if (error.length > 0) {
           res.send(JSON.stringify({ status: 'error', message: error }));
         } else {
           if (valid) {
-            exec("./grades.py -z \"" + config.salt + "\" -m '" + JSON.stringify({ username: data.username, key: 'password', value: data.new_password }) + "'", function (error, stdout, stderror) {
+            exec("./grades.py -z \"" + config.salt + "\" -m '" + JSON.stringify({ student_id: data.student_id, key: 'password', value: data.new_password }) + "'", function (error, stdout, stderror) {
               res.send(JSON.stringify({ status: 'ok', message: 'Your password has been updated.' }));
             });
           } else {
